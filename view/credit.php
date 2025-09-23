6ff03e32c9db47811f4227a5f7a4b590b100f293
Kromatique <florian.simon32320@gmail.com>
2025-09-23 10:15:14 +0200

    CSS V2
    Changement login et page de crédit (avec claude)

--- view/credit.php
+++ view/credit.php
@@ -1,9 +1,15 @@
 <h2>BTS SIO SLAM</h2>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier les Points</title>
+    <link rel="stylesheet" href="../src/css/credit.css">
+</head>
     <ul>
         <li><a href="#">1</a></li>
         <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="https://github.com/ImDarling-bit">Noah SAGOT</a></li>
+        <li><a href="#">SIMON Florian</a></li>
+        <li><a href="https://github.com/ImDarling-bit">SAGOT Noah</a></li>
     </ul>
     
 <h2>BTS SIO SISR</h2>
